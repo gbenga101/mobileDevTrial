15a91dd7e93f680c082033185ea9709781f77dae
Gbenga Hassan Fagbenro <43672597+gbenga101@users.noreply.github.com>
2024-10-11 19:29:13 +0100

    First DART class

---
+++ bin/dart_task_1.dart
@@ -0,0 +1,74 @@
+void main(){
+  /* Declare a variable to hold age and assign a value to it
+  Store what the age will be after 1 year in another */ 
+  int age = 24;
+  int nextYear = age + 1;
+  print(nextYear);
+
+  /*Create a variable (item price) and assign 29.99. 
+   Create another variable (discount) and assign value of 5. 
+   Print the price after removing the discount. */
+  double itemPrice = 29.99;
+  int discount = 5;
+  double totalDiscount = itemPrice - discount;
+  print("The total discount is $totalDiscount");
+  
+  /* Declare a variable to hold - Learning Dart is fun 
+   Declare a variable to hold your name with any digit (e.g Oluwaseyifunmi07) 
+   Correct(Debug) this line of code – string first name = John */
+  String fact = "Learning Dart is fun";
+  String name = "uniqueGbengah";
+  String first_name = "Gbenga";
+  print("My name is $name, and $fact");
+
+  /* Question 4
+   Assuming we have – int number = 10, create a double variable to hold what the value of number will be when passed to it 
+   Initialize a variable that stores your name */
+  int number = 10;
+  double task = number/3;
+  String Name = "Gbengah";
+  print("The TOTAL value is $task, calculated by $Name");
+
+  /*Question 5 
+   Initialize 2 double variables 
+   Initialize a variable to hold the sum of the 2 values */
+  double firstNumber = 125.05;
+  double secondNumber = 54.50;
+  double sum = firstNumber - secondNumber;
+  print(sum);
+
+  /*Question 6 
+   Declare int num to hold 5, assign 203 to the same variable 
+   Correct this line of code – int number = 2.9; */
+  int num = 5;
+  num = 203;
+  double Number = 2.9;
+  print("$num and $Number");
+
+  /*Question 7 
+   Within a multiline comment, Explain the role of main function in dart 
+   Within a multiline comment, give 5 rules guiding a variable name declaration  
+   Within a multiline comment, explain the use of comment within your code */
+
+
+  /*Question 8 
+   What type of Operator is +=  (This operator is used to add and display the value at the same time)
+   Assign a value to a variable named num. Use the += to add 3 to num 
+   Showcase an example with *= and /= */
+  int NUM = 12;
+  NUM +=3;
+  print(NUM);
+
+  int NuM = 12;
+  NuM *=3;
+  print(NuM);
+
+  double nuM = 14;
+  nuM /=3;
+  print(nuM);
+
+  /*Question 9 
+   Within a multiline comment, explain the difference between the data types discussed so far 
+   Within a multiline comment, explain the values that a Boolean variable can hold*/
+
+}
---
+++ test/mobile_trial_test.dart
@@ -0,0 +1,8 @@
+import 'package:mobile_trial/mobile_trial.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
